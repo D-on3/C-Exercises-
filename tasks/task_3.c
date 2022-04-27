afdbf2e952d7c797607043d7dd5cf764fde340b6
D-on3 <dnedyalkov396@gmail.com>
2022-04-28 00:28:23 +0300

    init

---
+++ tasks/task_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(){
+
+int choosen;
+
+printf("What is your favorite NUMBER:\n");
+scanf("%d",&choosen);
+
+printf("\nReally %d is my favorite number too!\n", choosen);
+return 0;
+}
+
+
