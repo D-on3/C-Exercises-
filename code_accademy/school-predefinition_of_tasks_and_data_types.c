34adb09b5316b76fd58617bbd324184c56bce923
D-on3 <dnedyalkov396@gmail.com>
2022-04-28 17:21:44 +0300

    init

---
+++ code_accademy/school-predefinition_of_tasks_and_data_types.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(){
+    //Input data-types //
+    char cFirstName ;
+    char cLastName ;
+    char cPhoneNumber ;
+    char cClass ;
+    float dAvarageGrade ;
+    //Also for sorting the data we will use arrays//
+    float grades = {}
+    //Finaly after the end of the period of// 
+    //study program we will calculate the avarage grade//
+} 
