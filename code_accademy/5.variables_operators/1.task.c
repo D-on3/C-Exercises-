29cb9e99d27dc75b5a9eaf283cc64f191a6ef013
D-on3 <dnedyalkov396@gmail.com>
2022-04-29 21:22:10 +0300

    init

---
+++ code_accademy/5.variables_operators/1.task.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+ int iVar = -250 ;
+ int iVar2 = 250 ;
+ int iVar3 = 4 ;
+ long int iVar4 = 589498 ;
+ unsigned long long int iVar5 = 10000000000000000000u ;
+ signed long long int iVar6 =   -9000000000000000000 ;
+
+ printf("%d\n",iVar);
+ printf("%d\n",iVar2);
+ printf("%d\n",iVar3);
+ printf("%lu\n",iVar4);
+ printf("%llu\n",iVar5);
+ printf("%lli\n",iVar6);
+ 
+ return 1;
+
+    
+}
