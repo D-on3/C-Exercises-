5738fe7fefd9440bc518ba97d163763c61ef012e
D-on3 <dnedyalkov396@gmail.com>
2022-04-30 07:06:26 +0300

    init

---
+++ code_accademy/5.variables_operators/10.task.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/*Задача 10
+▪ Намислете си цяло число от 1 до 10. Създайте променлива и я
+иницализирайте с него.
+▪ Съберете го с 5.
+▪ Получената стойност умножете по 4.
+▪ От резултата извадете 3 и разделете на 2.
+▪ Принтирайте крайната стойност на променливата на екрана.
+Отговаря ли резултатът на очакванията ви?*/
+int main(){
+
+int iWholeNumber = 6;
+int iFinalNumber ;
+iFinalNumber = iWholeNumber + 5;
+iFinalNumber = iFinalNumber * 4 ;
+iFinalNumber = (iFinalNumber -3) / 2; 
+printf("Final result is : %d ")
+
+}
