249d28a431d622d635ac975e30bdb1c08d3a595d
D-on3 <dnedyalkov396@gmail.com>
2022-05-09 21:38:42 +0300

    init

---
+++ code_accademy/6.input_output/3.task.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+
+int a;
+int c; 
+//Limiting the input to 4 symbols
+scanf_s("%4d",&a);
+//Limiting the input to 5 symbols 
+scanf_s("%5d",&c);
+//Printing the output
+printf("%d %d",a,c);
+return 0;
+
+}
