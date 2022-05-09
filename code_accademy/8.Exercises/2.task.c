08d227da13d10b270c056b983bc5760121335d25
D-on3 <dnedyalkov396@gmail.com>
2022-05-09 21:39:57 +0300

    init

---
+++ code_accademy/8.Exercises/2.task.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+int x =1 ;
+if(x<= 10) {
+printf("%d\n",x);
+x +=1;
+}
+if(x<= 10) {
+printf("%d\n",x);
+x +=1;
+}
+if(x<= 10) {
+printf("%d\n",x);
+x +=1;
+}
+if(x<= 10) {
+printf("%d\n",x);
+x +=1;
+}
+if(x<= 10) {
+printf("%d\n",x);
+x +=1;
+if(x<= 10) {
+printf("%d\n",x);
+x +=1;
+}
+if(x<= 10) {
+printf("%d\n",x);
+x +=1;
+}
+if(x<= 10) {
+printf("%d\n",x);
+x +=1;
+}
+if(x<= 10) {
+printf("%d\n",x);
+x +=1;
+}
+if(x<= 10) {
+printf("%d\n",x);
+x +=1;
+}
+return 0;
+}
+
+
+
+}
